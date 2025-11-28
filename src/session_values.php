de17b9da6fca8df5f6907b6b793a1979a4f770f6
ikegayasoshisenshu-bit <ikegayasoshi.senshu@gmail.com>
2025-11-28 11:04:08 +0900

    WebMessageBorad

---
+++ src/session_values.php
@@ -0,0 +1,38 @@
+<?php
+
+$messages['action_success_text'] = '';
+$messages['action_error_text'] = '';
+$messages['input_error_author_name'] = '';
+$messages['input_error_message'] = '';
+$messages['input_pre_author_name'] = '';
+$messages['input_pre_message'] = '';
+
+if (isset($_SESSION['action_success_text'])) {
+    $messages['action_success_text'] = $_SESSION["action_success_text"];
+    unset($_SESSION["action_success_text"]);
+}
+
+if (isset($_SESSION['action_error_text'])) {
+    $messages['action_error_text'] = $_SESSION["action_error_text"];
+    unset($_SESSION["action_error_text"]);
+}
+
+if (isset($_SESSION['input_error_author_name'])) {
+    $messages['input_error_author_name'] = $_SESSION["input_error_author_name"];
+    unset($_SESSION["input_error_author_name"]);
+}
+
+if (isset($_SESSION['input_error_message'])) {
+    $messages['input_error_message'] = $_SESSION["input_error_message"];
+    unset($_SESSION["input_error_message"]);
+}
+
+if (isset($_SESSION['input_pre_author_name'])) {
+    $messages['input_pre_author_name'] = $_SESSION["input_pre_author_name"];
+    unset($_SESSION["input_pre_author_name"]);
+}
+
+if (isset($_SESSION['input_pre_message'])) {
+    $messages['input_pre_message'] = $_SESSION["input_pre_message"];
+    unset($_SESSION["input_pre_message"]);
+}
